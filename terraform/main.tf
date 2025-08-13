b86df5932dcf7c2c1044a33379d3e8773eebf220
Zufayri <64583766+Zufayri@users.noreply.github.com>
2025-08-13 11:43:13 +0800

    Feature/security group (#1)
    * Update variables for /main.tf, tf.vars, variables.tf, modules/security-group/main
    
    * Delete readme.md in terraform folder
    
    * feat(security-group): Security Group for ALB, EC2/nginx, EC2/webapp
    
    * Fixed: Rename networking to network in /main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,15 @@
-module "networking" {
+module "network" {
     source = "./modules/network"
     vpc_cidr = var.vpc_cidr
     vpc_name = var.vpc_name
     cidr_public_subnet = var.cidr_public_subnet
     aws_availability_zone = var.aws_availability_zone
     cidr_private_subnet = var.cidr_private_subnet 
+}
+
+module "sg" {
+    source = "./modules/security_group"
+    vpc_id = module.network.vpc_id
+    allowed_ssh = var.allowed_ssh_cidr
 }
 
---
+++ terraform/modules/security_group/main.tf
@@ -0,0 +1,100 @@
+# Variables
+variable "vpc_id" {type = string}
+variable "allowed_ssh" {type = string}
+
+# ALB Security Group (Inbound:80,443 , Outbound:ALL)
+resource "aws_security_group" "alb_sg" {
+    name = "alb-sg"
+    vpc_id = var.vpc_id
+    description = "ALB SG: Inbound:80,443 , Outbound:ALL "
+
+    # Firewall Rules
+    ingress {
+        description = "Allow HTTP from everywhere"
+        from_port = 80 
+        to_port = 80 
+        protocol = "tcp" 
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "Allow HTTPS from everywhere"
+        from_port = 443 
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "Allow outgoing request"
+        from_port = 0 
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    tags = { Name = "alb-sg"} 
+
+}
+
+# EC2/Nginx Security Group
+resource "aws_security_group" "nginx_sg" {
+    name = "nginx-sg"
+    vpc_id = var.vpc_id
+    description = "Nginx SG"
+
+    # Firewall Rules
+    ingress {
+        description = "Allow HTTP from ALB"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        security_groups = [aws_security_group.alb_sg.id] #call from terraform resource named alb_sg
+        }
+
+    ingress {
+        description = "Allow SSH from allowed_ssh IP"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.allowed_ssh]
+        }
+
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {Name = "nginx-sg"}
+
+}
+
+# EC2/Webapp Security Group
+resource "aws_security_group" "webapp_sg" {
+    name = "webapp-sg"
+    vpc_id = var.vpc_id
+    description = "Webapp-SG: Inbound:5000 , Outbound:ALL"
+
+    #Firewall rules
+    ingress {
+        from_port = 5000
+        to_port = 5000
+        protocol = "tcp"
+        security_groups = [aws_security_group.nginx_sg.id] #call from terraform resource named nginx_sg
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {Name = "webapp-sg"}
+}
+
+
+
