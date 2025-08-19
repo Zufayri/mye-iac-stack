0bd1aac9aac1a26e6d14e45d1623db813271abc9
zufayri <mzufayri55@gmail.com>
2025-08-19 08:30:35 +0800

    feat(ec2): add ec2-nginx module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Network
 module "network" {
     source = "./modules/network"
     vpc_cidr = var.vpc_cidr
@@ -7,9 +8,37 @@ module "network" {
     cidr_private_subnet = var.cidr_private_subnet 
 }
 
+# Security Group
 module "sg" {
     source = "./modules/security_group"
     vpc_id = module.network.vpc_id
     allowed_ssh = var.allowed_ssh_cidr
 }
 
+# AMI
+data "aws_ami" "ubuntu_latest" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Instances
+module "nginx" {
+    source = "./modules/ec2"
+    name = "nginx"
+    subnet_id = module.network.private_subnet_ids[0]
+    instance_type = "t3.micro"
+    key_name = "my-key"  # This is existing manually created key. Change its name if required.
+    security_group_ids = [module.sg.nginx_sg_id] #[module.sg.nginx_sg_id]
+    associate_public_ip = true # Change to false if utilizing private subnet 
+    ami_id = data.aws_ami.ubuntu_latest.id  #Insert AMI ID
+    }
---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,19 @@
+# Variables
+variable "name" {type =string}
+variable "subnet_id" {type=string}
+variable "instance_type" {type= string}
+variable "key_name" {type=string}
+variable "security_group_ids" {type=list(string)}
+variable "associate_public_ip" {type=string}
+variable "ami_id" {type = string}
+
+
+resource "aws_instance" "main" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = var.security_group_ids
+    associate_public_ip_address = var.associate_public_ip 
+    tags = {Name = var.name}
+}
+
